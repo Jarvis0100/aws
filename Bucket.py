59adb5cb4a2da76a64372da16c6e66afd91361d2
Jarvis0100 <51711169+Jarvis0100@users.noreply.github.com>
2024-07-05 12:01:53 +0530

    Update Bucket.py

--- Bucket.py
+++ Bucket.py
@@ -1,4 +1,27 @@
 import boto3
+from botocore.exceptions import ClientError
+
+def get_secret():
+    secret_name = "godsent"
+    region_name = "ap-south-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        print(e)
+        return None
+
+    secret = get_secret_value_response['SecretString']
+    return secret
 
 s3 = boto3.client('s3')
 
@@ -7,11 +30,13 @@ def list_objects(sb):
         objects = s3.list_objects(Bucket=sb)['Contents']
     except Exception as e:
         print("Error while fetching:", e)
+        return None
+
     return objects
 
 def copy_objects(ob, sb, db):
+    files = []
     try:
-        files = []
         for obj in ob:
             objkey = obj['Key']
             files.append(objkey)
@@ -21,6 +46,8 @@ def copy_objects(ob, sb, db):
             print("file successfully uploaded at {}".format(db))
     except Exception as e:
         print(e)
+        return None
+
     return files
 
 def delete_objects(sb, files):
@@ -32,10 +59,12 @@ def main():
     sourcebucket = 'aplhabucket123'
     destinationbucket = 'destinationbucketajay'
     objs = list_objects(sourcebucket)
-    files = copy_objects(objs, sourcebucket, destinationbucket)
-    print("All files successfully copied into Destination bucket")
-    delete_objects(sourcebucket, files)
-    print("All files Deleted from Source Bucket")
+    if objs is not None:
+        files = copy_objects(objs, sourcebucket, destinationbucket)
+        if files is not None:
+            print("All files successfully copied into Destination bucket")
+            delete_objects(sourcebucket, files)
+            print("All files Deleted from Source Bucket")
 
 if __name__ == '__main__':
     main()
